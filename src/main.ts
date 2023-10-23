3e1291471bcfe58b8fb2d6bb93fbd5f85614c72d
sq <wweerrbb@gmail.com>
2023-10-23 16:25:54 -0400

    profilePicZoom not wokring

--- src/main.ts
+++ src/main.ts
@@ -6,24 +6,33 @@ const crawler = new PlaywrightCrawler({
 
         const username = await page.locator('.b-compact-header__wrapper').textContent();
         const followers = await page.locator('svg[data-icon-name="icon-follow"] use')
-        .getAttribute('xlink:href');
+        // .getAttribute('xlink:href');
         const profilePic = await page.locator('.g-avatar__img-wrapper img').getAttribute('src');
+        const profilePicZoom = await page.locator('.pswp__img');
         const userInfo = await page.locator('.b-user-info__text p').textContent();
         const banner = await page.locator('.b-profile__header img').getAttribute('src');
+        const subsFee = await page.locator('.b-offer-join').first().textContent();
         // Save data to default dataset
         await Dataset.pushData({
+            subsFee: subsFee,
             url: request.url,
             username: username,
             followers: followers,
             avatar: profilePic,
+            avatarZoom: profilePicZoom,
             banner: banner,
             userInfo: userInfo,
         });
     },
 });
 
+// <div classname="abc">
+    //  <div/>
+    //  <img/>
+    // </div>
+
 await crawler.addRequests([
-    'https://onlyfans.com/brandyontherocks'
+    'https://onlyfans.com/loonascandi'
 ]);
 
 // Run the crawler
