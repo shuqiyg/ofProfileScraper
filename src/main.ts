170f1a40062f622aa3f2051058c9af62a1e42682
sq <wweerrbb@gmail.com>
2023-10-26 16:22:35 -0400

    search for other social media links

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,14 @@ import fs from 'fs';
 
 const crawler = new PlaywrightCrawler({
     // Function called for each URL
-    async requestHandler({ request, page, body }) {
+    async requestHandler({ request, page, enqueueLinks}) {
         maxRequestsPerCrawl:100;
         //username 
         const username = await page.locator('.b-compact-header__wrapper').textContent();
+
+        //account name
+        const splits = request.url.split('/');
+        const accountName = splits[splits.length - 1];
 
         // stats of images, videos, likes, views, and streams
         const lis = await page.$$('.b-profile__sections li');
@@ -19,9 +23,9 @@ const crawler = new PlaywrightCrawler({
             console.log(`Icon name: ${iconName}, count: ${countSpan}`);
         }
         console.log("****", stats)
-        const userStats = await page.locator('.b-profile__sections').innerText();
+        // const userStats = await page.locator('.b-profile__sections').innerText();
 
-        // const followers = await page.locator('svg[data-icon-name="icon-follow"] use');
+        const followers = await page.locator('svg[data-icon-name="icon-follow"] use');
 
         //need to get the price of subscription, either free or $XX.XX
         const subsFee = await page.locator('.b-offer-join').first().textContent();
@@ -34,40 +38,53 @@ const crawler = new PlaywrightCrawler({
         //change the url to the full size image
         const profilePicZoom = convertThumbnailToPublic(profilePic as string);
         //get the user info (bio)
-        const userInfo = await page.locator('.b-user-info__text p').textContent();
+        const userBio = await page.locator('.b-user-info__text p').textContent();
 
         //personal link
         const personalLink = await page.locator('.b-user-info__detail a').getAttribute('href');
 
         //general profile content(html)
         const profile = await page.locator('.b-profile__content').innerHTML();
-        
+
+        //search for other social media links (tiktok, intagram, twitter, facebook, fansly, etc)
+    //     console.log(request.url);
+    //     await page.waitForSelector('.swiper-wrapper a');
+    //     const links = await page.$$eval('.swiper-wrapper a', links => links.map(link => link.getAttribute('href')));
+    //     if (links.length > 0) {
+    //       await enqueueLinks({
+    //           urls: links.filter(link => link !== null) as string[],
+    //           selector: 'a',
+    //           label: 'page',
+    //           requestQueue: crawler.requestQueue
+    //       });
+    //   }
+
+    
         // Save data to default dataset
         await Dataset.pushData({
-            subsFee: findPriceOrFree(subsFee??"Free"),
-            // prict:price,
             onlyFansUrl: request.url,
-            accountName: username,
-            username: "",
+            accountName,
+            userName: username,
+            subsFee: findPriceOrFree(subsFee??"Free"),
             stats,
-            userStats: userStats,
-            // followers: followers,
+            followers: followers,
             avatar: profilePic,
             avatarZoom: profilePicZoom,
             bannerImg: banner,
-            userBio: userInfo,
+            userBio: userBio,
             location: "",
             links: personalLink,
             profileHtml: profile,
         });
+        
     },
 });
 
     // scrape 50 pages at a time, need to find out the limits of crawlee, maybe write a script to run multiple crawlers at once
     await crawler.addRequests([
-        // 'https://onlyfans.com/elaina_stjames',
-        // 'https://onlyfans.com/loonascandi',
-        // 'https://onlyfans.com/laurenelizabeth',
+        'https://onlyfans.com/elaina_stjames',
+        'https://onlyfans.com/loonascandi',
+        'https://onlyfans.com/laurenelizabeth',
     ]);
 
 //helper 1
